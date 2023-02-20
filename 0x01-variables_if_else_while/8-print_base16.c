648844e3c880d559f1f9b30868a30dd2d5b9f991
Petro <pedro.tiwa@gmail.com>
2023-02-20 06:45:22 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
-	for (alphabet = 'a'; alphabet <= 'j'; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
 		putchar(alphabet);
 	putchar('\n');
 	return (0);
